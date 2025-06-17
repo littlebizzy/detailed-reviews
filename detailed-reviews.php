c437f95ac6f2249a4c941e9f1fa544d4ccdecd40
Jesse Nickles <jesse@littlebizzy.com>
2025-06-17 22:22:49 +0700

    Create detailed-reviews.php

---
+++ detailed-reviews.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Detailed Reviews
+Plugin URI: https://www.littlebizzy.com/plugins/force-https
+Description: Allows 5-star reviews with multiple categories per post. Compatible with legacy WP Review Site data.
+Version: 1.0.0
+Author: LittleBizzy
+Author URI: https://www.littlebizzy.com
+Requires PHP: 7.0
+Tested up to: 6.7
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+Update URI: false
+GitHub Plugin URI: littlebizzy/detailed-reviews
+Primary Branch: master
+Text Domain: detailed-reviews
+*/
